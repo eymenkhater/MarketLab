772f5f89e1067ffaa94756474c5d5684cb47a9d3
Eymen Khater <writerustyle@Eymens-MacBook-Pro.local>
2020-03-13 19:12:43 +0300

    data quey list added.

--- src/MarketLab.Application/Core/Extensions/DataQueryExtension.cs
+++ src/MarketLab.Application/Core/Extensions/DataQueryExtension.cs
@@ -1,7 +1,21 @@
+using System.Linq;
+using System.Collections.Generic;
+using MarketLab.Application.Core.Extensions.Queries;
+using MarketLab.Application.Core.Queries.Primitives;
+
 namespace MarketLab.Application.Core.Extensions
 {
     public static class DataQueryExtension
     {
+        public static IEnumerable<T> ToDataQueryList<T>(this IEnumerable<T> data, DataQuery query)
+        {
+            data = data.Search(query.Searching)
+                        .Filter(query.Filtering)
+                        .Sort(query.Sorting);
 
+            query.Paging.Total = data?.Count() ?? 0;
+
+            return data.ToPagedList(query.Paging);
+        }
     }
 }
